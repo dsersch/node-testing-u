2c24caa42e165f010c85f821683e1b1fa18a117c
David Sersch <dsersch640@gmail.com>
2018-08-16 13:15:08 -0700

    lesson 53

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,33 @@
+const expect = require('expect');
 const utils = require('./utils.js');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    if (res != 44) {
-        throw new Error(`Expected 44, but got ${res}`)
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var res = utils.square(2)
 
-    if (res != 4) {
-        throw new Error(`Expected 4, but got ${res}`)
-    }
+    expect(res).toBe(4).toBeA('number');
 })
+
+it('should verify names are set', () => {
+    var user = utils.setName({age: 33, location: 'Los Angeles'}, 'Dave Sersch')
+
+    expect(user).toInclude({firstName: 'Dave', lastName: 'Sersch'});
+    expect(user.firstName).toBeA('string');
+    expect(user.lastName).toBeA('string');
+})
+
+// it('should expect some values', () => {
+//     //expect(12).toNotBe(11);
+//     //expect({name: 'dave'}).toEqual({name: 'dave'});
+//     expect([2, 3, 4, 5]).toInclude(5)
+//     expect({
+//         name: 'Dave',
+//         age: 33,
+//         location: "my location"
+//     }).toExclude({age: 31})
+// })
